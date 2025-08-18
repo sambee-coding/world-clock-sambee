a45fc9a687a606393160b3a8d20b6598840ae856
sambee coding <samrisamrawit30@@email.com>
2025-08-18 02:14:09 -0700

    i made change

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
+
+function updateTime(){
 let addisAbebaElement=document.querySelector("#addis");
-let addisSpost=addisAbebaElement.querySelector(".name-spot")
 let addisDateElement=addisAbebaElement.querySelector(".date");
 let addisTimeElement=addisAbebaElement.querySelector(".time");
 let addisTime=moment();
- 
+
 addisDateElement.innerHTML=addisTime.format("MMMM Do YYYY");
- addisTimeElement.innerHTML=`${addisTime.format("h:mm:ss")} <small>${addisTime}`
+ addisTimeElement.innerHTML=`${addisTime.format("h:mm:ss")} <small> ${addisTime.format("A")} </small>`;
+}
+updateTime();
+setInterval(updateTime,1000);
+ 
 
